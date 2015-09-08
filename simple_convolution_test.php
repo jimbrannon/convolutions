bb0e2f6a021340fff6451cbd7c1935b7dcea51d8
Jim Brannon <jim@brannondevelopments.com>
2015-09-08 00:21:44 -0600

    close tag in both files

--- simple_convolution_test.php
+++ simple_convolution_test.php
@@ -2,11 +2,11 @@
 /*
  * test the simple convolution function
  */
-include 'simple_convolution.php';
+include("simple_convolution.php");
 
 $excitation_array=array(2001=>5.0,2002=>10.0,2003=>5.0);
 print_r($excitation_array);
 $response_array=array(1=>0.1,2=>0.5,3.>0.4);
 print_r($response_array);
 print_r(simple_convolution($excitation_array,$response_array));
-
+?>

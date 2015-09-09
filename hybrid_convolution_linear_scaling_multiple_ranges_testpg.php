bc750b472d614372a5fa6dc308129d82f5d3c7e3
Jim Brannon <jim@brannondevelopments.com>
2015-09-09 00:00:15 -0600

    added more CLI and URL args, also better value checking logic

--- hybrid_convolution_linear_scaling_multiple_ranges_testpg.php
+++ hybrid_convolution_linear_scaling_multiple_ranges_testpg.php
@@ -23,6 +23,10 @@ define("RGMODELVERSION","rgmodelversion");
 define("RGRESPONSEZONE","rgresponsezone");
 define("RGSTREAMREACH","rgstreamreach");
 
+define("RGRESPFNFTABLE","rgrespfntable");
+define("RGRESPFNDATATABLE","rgrespfndatatable");
+define("RGGWNETCUYRTABLE","rggwnetcuyrtable");
+
 /*
  * argument defaults
  *   these are OK to modify, as long as the values are valid
@@ -35,7 +39,6 @@ $options[LOGGING]=true;
 
 $options[PGUSER]="drupal";
 $options[PGPASSWORD]="drupal_psql";
-$options[PGTABLE]="empty_dummy";
 $options[PGDB]="d7dev";
 $options[PGHOST]="localhost";
 $options[PGPORT]=5432;
@@ -43,6 +46,9 @@ $options[PGPORT]=5432;
 $options[RGMODELVERSION]=4; // 4 is 6P97
 $options[RGRESPONSEZONE]=1; // 1 is SD1
 $options[RGSTREAMREACH]=1; // 1 is RG1 (del norte to excelsior)
+$options[RGRESPFNFTABLE]="rg_response_functions";
+$options[RGRESPFNDATATABLE]="rg_response_function_data";
+$options[RGGWNETCUYRTABLE]="rg_response_zone_model_data_annual";
 
 /*
  * handle the args right here in the wrapper
@@ -57,7 +63,7 @@ if (array_key_exists(DEBUGGING,$options)) {
 	$debugging = false;
 }
 if ($debugging) echo "debugging default: $debugging \n";
-$debugging_arg = getargs ("debugging",$debugging);
+$debugging_arg = getargs (DEBUGGING,$debugging);
 if ($debugging) echo "debugging_arg: $debugging_arg \n";
 if (strlen(trim($debugging_arg))) {
 	$debugging = strtobool($debugging_arg);
@@ -73,7 +79,7 @@ if (array_key_exists(LOGGING,$options)) {
 	$logging = false;
 }
 if ($debugging) echo "logging default: $logging \n";
-$logging_arg = getargs ("logging",$logging);
+$logging_arg = getargs (LOGGING,$logging);
 if ($debugging) echo "logging_arg: $logging_arg \n";
 if (strlen(trim($logging_arg))) {
 	$logging = strtobool($logging_arg);
@@ -89,7 +95,7 @@ if (array_key_exists(PGUSER,$options)) {
 	$pguser = "";
 }
 if ($debugging) echo "pguser default: $pguser \n";
-$pguser_arg = getargs ("pguser",$pguser);
+$pguser_arg = getargs (PGUSER,$pguser);
 if ($debugging) echo "pguser_arg: $pguser_arg \n";
 if (strlen(trim($pguser_arg))) {
 	$pguser = trim($pguser_arg);
@@ -107,7 +113,7 @@ if (array_key_exists(PGPASSWORD,$options)) {
 	$pgpassword = "";
 }
 if ($debugging) echo "pgpassword default: $pgpassword \n";
-$pgpassword_arg = getargs ("pgpassword",$pgpassword);
+$pgpassword_arg = getargs (PGPASSWORD,$pgpassword);
 if ($debugging) echo "pgpassword_arg: $pgpassword_arg \n";
 if (strlen(trim($pgpassword_arg))) {
 	$pgpassword = trim($pgpassword_arg);
@@ -115,33 +121,6 @@ if (strlen(trim($pgpassword_arg))) {
 	if ($debugging) echo "pgpassword final: $pgpassword \n";
 } else {
 	if ($debugging) echo "pgpassword final: $pgpassword \n";
-}
-/*
- * get the pgtable arg
- * this is required, so bail if it is not set from either the default above or the cli arg
- */
-if (array_key_exists(PGTABLE,$options)) {
-	$pgtable = $options[PGTABLE];
-} else {
-	// we can NOT set a default for this so the arg better have something!
-	$pgtable = "";
-}
-if ($debugging) echo "pgtable default: $pgtable \n";
-$pgtable_arg = getargs ("pgtable",$pgtable);
-if ($debugging) echo "pgtable_arg: $pgtable_arg \n";
-if (strlen(trim($pgtable_arg))) {
-	$pgtable = trim($pgtable_arg);
-	$options[PGTABLE] = $pgtable;
-	if ($debugging) echo "pgtable final: $pgtable \n";
-} else {
-	if (strlen(trim($pgtable))) {
-		if ($debugging) echo "pgtable final: $pgtable \n";
-	} else {
-		// we can NOT proceed without a pgtable!!
-		if ($logging) echo "Error: Missing pgtable. \n";
-		if ($debugging) print_r($options);
-		return false;
-	}
 }
 /*
  * get the pgdb arg
@@ -154,7 +133,7 @@ if (array_key_exists(PGDB,$options)) {
 	$pgdb = "";
 }
 if ($debugging) echo "pgdb default: $pgdb \n";
-$pgdb_arg = getargs ("pgdb",$pgdb);
+$pgdb_arg = getargs (PGDB,$pgdb);
 if ($debugging) echo "pgdb_arg: $pgdb_arg \n";
 if (strlen(trim($pgdb_arg))) {
 	$pgdb = trim($pgdb_arg);
@@ -181,7 +160,7 @@ if (array_key_exists(PGHOST,$options)) {
 	$pghost = "localhost";
 }
 if ($debugging) echo "pghost default: $pghost \n";
-$pghost_arg = getargs ("pghost",$pghost);
+$pghost_arg = getargs (PGHOST,$pghost);
 if ($debugging) echo "pghost_arg: $pghost_arg \n";
 if (strlen(trim($pghost_arg))) {
 	$pghost = trim($pghost_arg);
@@ -201,7 +180,7 @@ if (array_key_exists(PGPORT,$options)) {
 	$pgport = 5432;
 }
 if ($debugging) echo "pgport default: $pgport \n";
-$pgport_arg = getargs ("pgport",$pgport);
+$pgport_arg = getargs (PGPORT,$pgport);
 if ($debugging) echo "pgport_arg: $pgport_arg \n";
 if (strlen(trim($pgport_arg))) {
 	$pgport = intval($pgport_arg);
@@ -212,13 +191,115 @@ if (strlen(trim($pgport_arg))) {
 }
 
 /*
- * get data from pg
+ * get the rg model version arg
+ * this is required, so bail if it is not set from either the default above or the cli arg
  */
-$pgdb = "d7dev";
-$pghost = "localhost";
-$pgport = 5432;
-$pguser = "drupal";
-$pgpassword = "drupal_psql";
+if (array_key_exists(RGMODELVERSION,$options)) {
+	$rgmodelversion = $options[RGMODELVERSION];
+} else {
+	// we can not set a default for this
+	$rgmodelversion = 0; // set it to an invalid value and check later
+}
+if ($debugging) echo "rgmodelversion default: $rgmodelversion \n";
+$rgmodelversion_arg = getargs (RGMODELVERSION,$rgmodelversion);
+if ($debugging) echo "rgmodelversion_arg: $rgmodelversion_arg \n";
+if (strlen(trim($rgmodelversion_arg))) {
+	$rgmodelversion = intval($rgmodelversion_arg);
+}
+if ($rgmodelversion > 0) {
+	// a potentially valid value, use it
+	if ($debugging) echo "final rgmodelversion: $rgmodelversion \n";
+	$options[RGMODELVERSION] = $rgmodelversion;
+} else {
+	// can not proceed without this
+    if ($logging) echo "invalid rgmodelversion: $rgmodelversion exiting \n";
+	if ($debugging) echo "invalid rgmodelversion: $rgmodelversion exiting \n";
+	return;
+}
+/*
+ * get the rg response zone arg
+ * this is required, so bail if it is not set from either the default above or the cli arg
+ */
+if (array_key_exists(RGRESPONSEZONE,$options)) {
+	$rgresponsezone = $options[RGRESPONSEZONE];
+} else {
+	// we can not set a default for this
+	$rgresponsezone = 0; // set it to an invalid value and check later
+}
+if ($debugging) echo "rgresponsezone default: $rgresponsezone \n";
+$rgresponsezone_arg = getargs (RGRESPONSEZONE,$rgresponsezone);
+if ($debugging) echo "rgresponsezone_arg: $rgresponsezone_arg \n";
+if (strlen(trim($rgresponsezone_arg))) {
+	$rgresponsezone = intval($rgresponsezone_arg);
+}
+if ($rgresponsezone > 0) {
+	// a potentially valid value, use it
+	if ($debugging) echo "final rgresponsezone: $rgresponsezone \n";
+	$options[RGRESPONSEZONE] = $rgresponsezone;
+} else {
+	// can not proceed without this
+	if ($logging) echo "invalid rgresponsezone: $rgresponsezone exiting \n";
+	if ($debugging) echo "invalid rgresponsezone: $rgresponsezone exiting \n";
+	return;
+}
+/*
+ * get the rg stream reach arg
+ * this is required, so bail if it is not set from either the default above or the cli arg
+ */
+if (array_key_exists(RGSTREAMREACH,$options)) {
+	$rgstreamreach = $options[RGSTREAMREACH];
+} else {
+	// we can not set a default for this
+	$rgstreamreach = 0; // set it to an invalid value and check later
+}
+if ($debugging) echo "rgstreamreach default: $rgstreamreach \n";
+$rgstreamreach_arg = getargs (RGSTREAMREACH,$rgstreamreach);
+if ($debugging) echo "rgstreamreach_arg: $rgstreamreach_arg \n";
+if (strlen(trim($rgstreamreach_arg))) {
+	$rgstreamreach = intval($rgstreamreach_arg);
+}
+if ($rgstreamreach > 0) {
+	// a potentially valid value, use it
+	if ($debugging) echo "final rgstreamreach: $rgstreamreach \n";
+	$options[RGSTREAMREACH] = $rgstreamreach;
+} else {
+	// can not proceed without this
+	if ($logging) echo "invalid rgstreamreach: $rgstreamreach exiting \n";
+	if ($debugging) echo "invalid rgstreamreach: $rgstreamreach exiting \n";
+	return;
+}
+
+/*
+ * get the rgrespfntable arg
+ * this is required, so bail if it is not set from either the default above or the cli arg
+ */
+if (array_key_exists(RGRESPFNFTABLE,$options)) {
+	$rgrespfntable = $options[RGRESPFNFTABLE];
+} else {
+	// we can NOT set a default for this
+	$rgrespfntable = ""; // set it to an invalid value and check later
+}
+if ($debugging) echo "rgrespfntable default: $rgrespfntable \n";
+$rgrespfntable_arg = getargs (RGRESPFNFTABLE,$rgrespfntable);
+if ($debugging) echo "rgrespfntable_arg: $rgrespfntable_arg \n";
+if (strlen(trim($rgrespfntable_arg))) {
+	$rgrespfntable = trim($rgrespfntable_arg);
+}
+if (strlen($rgrespfntable=trim($rgrespfntable))) {
+	// a potentially valid value, use it
+	if ($debugging) echo "final rgrespfntable: $rgrespfntable \n";
+	$options[RGRESPFNFTABLE] = $rgrespfntable;
+} else {
+	// can not proceed without this
+	if ($logging) echo "missing rgrespfntable exiting \n";
+	if ($debugging) echo "missing rgrespfntable exiting \n";
+	return;
+}
+
+
+/*
+ * make the pg coonnection
+ */
 $pgconnectionstring = "dbname=$pgdb host=$pghost port=$pgport";
 if (strlen($pguser)) {
 	$pgconnectionstring .= " user=$pguser";
@@ -232,7 +313,7 @@ if (!$pgconnection) {
 	return;
 }
 
-//excitation array
+//set up the excitation array
 //$excitation_array=array(2001=>5.0,2002=>10.0,2003=>0.0,2004=>15.0);
 $pgtable = "rg_response_zone_model_data_annual";
 $results = pg_query($pgconnection, "SELECT nyear,netgwcutotal,gwrechargetotal FROM $pgtable WHERE model_version=4 AND nzone=1 ORDER BY nyear ASC");
